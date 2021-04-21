d2149a12b3e6df3a8ffaa02256b612c16ed9c5a7
a_m_anisul_huq <dodulhuq@gmail.com>
2021-04-20 22:42:54 -0400

    First Tree Problem Commit

---
+++ src/main/java/com/fdmgroup/TreeStructure/Tree.java
@@ -0,0 +1,19 @@
+package com.fdmgroup.TreeStructure;
+
+public class Tree<E> {
+
+	E element;
+	
+	Tree<E> left;
+	Tree<E> right;
+	
+	
+	public Tree(E element) {
+		super();
+		this.element = element;
+		this.left = null;
+		this.right = null;
+	}
+	
+	
+}
